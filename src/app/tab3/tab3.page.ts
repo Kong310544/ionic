e7c5aaeccd416ee00f0b3ca25354d3b584b03c61
Kong310544 <kong310544@gmail.com>
2022-02-21 22:20:34 +0700

    first commit

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+
+    profile =
+      {
+        Username:"kong@310544gmail.com",
+        Password:"seue3101",
+        Address:"นิสัยเป็นหมาโครตเท่ที่สุด แบบไม่มีไครเทียบไดบ้านเลขที่ 8/8 อำเภอกำแพงแสน จังหวัดนครปฐม "
+      }
+      
+
+    
+
+  constructor() {}
+
+}
